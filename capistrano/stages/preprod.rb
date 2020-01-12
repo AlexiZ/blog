9f7f4cf95c2d2c08f09deb297f3cdd1615bf76eb
Alexis <mazaudiera@gmail.com>
2020-01-12 20:28:15 +0100

    Downgraded php since 7.4 is not available in preprod server

--- capistrano/stages/preprod.rb
+++ capistrano/stages/preprod.rb
@@ -10,8 +10,8 @@ set :deploy_to, '/kunden/homepages/39/d655277423/htdocs/blog'
 set :branch, 'develop'
 
 set :composer_install_flags, '--no-dev --prefer-dist --no-interaction --quiet --optimize-autoloader --ignore-platform-reqs'
-set :composer_php, 'php7.1-cli'
+set :composer_php, 'php7.3-cli'
 
 SSHKit.config.command_map[:composer] = "#{fetch(:deploy_to)}/shared/composer.phar"
 SSHKit.config.command_map[:composer] = "#{shared_path.join("composer.phar")}"
-SSHKit.config.command_map[:php] = "php7.1-cli"
+SSHKit.config.command_map[:php] = "php7.3-cli"
