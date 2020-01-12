5556ed7cb6c64a2f03faffa8678475646749e5d8
Alexis <mazaudiera@gmail.com>
2020-01-12 18:53:03 +0100

    Updated preprod deployment

--- capistrano/stages/preprod.rb
+++ capistrano/stages/preprod.rb
@@ -5,7 +5,7 @@ set :ssh_port, 22
 server fetch(:domain), user: 'u87073604', roles: %w{web app db}
 
 set :deploy_to, '/kunden/homepages/39/d655277423/htdocs/blog'
-set :branch, 'master'
+set :branch, 'develop'
 
 set :composer_install_flags, '--no-dev --prefer-dist --no-interaction --quiet --optimize-autoloader --ignore-platform-reqs'
 set :composer_php, 'php7.1-cli'
